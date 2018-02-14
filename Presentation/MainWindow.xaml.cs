4c8574903b8ee6a8485c89d14e4a1610e4b68d83
Daniel-C-Dias <danielcanastro@gmail.com>
2018-02-14 17:16:02 +0000

    Added UI Project (with erros)

---
+++ Presentation/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Presentation
+{
+    using System.IO;
+    using System.Windows;
+    using Microsoft.Win32;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
